c29c5cc6a284ae62cf74a0733397c438cf0ff49a
Tkachenko01001 <x380999660502@gmail.com>
2024-01-11 17:36:29 +0200

    Update ButtonCategory.tsx

--- src/app/components/button-category/ButtonCategory.tsx
+++ src/app/components/button-category/ButtonCategory.tsx
@@ -5,23 +5,28 @@ import { ButtonCategoryProps } from "@/app/types/types";
 import { setPage } from "@/redux/features/product-slice";
 import { setCategory } from "@/redux/features/categories-slice";
 import { CategorySingle } from "@/app/types/types";
-import { fetchProductsByCategory } from "@/redux/operations/Operations";
+import { fetchProductsByCategory, fetchProducts } from "@/redux/operations/Operations";
 
 const ButtonCategory: React.FC<ButtonCategoryProps> = ({ category }) => {
   const { selectedCategory } = useSelector(selectCategories);
   const dispatch = useAppDispatch();
 
-  const handleClickForCategory = (e: React.MouseEvent<HTMLButtonElement>, category: CategorySingle) => {
-    e.preventDefault();
-    
-    if (category === selectedCategory) {
-      dispatch(setCategory(''));
-      return;
-    }
-    dispatch(setCategory(category));
-    dispatch(setPage(1));
-    dispatch(fetchProductsByCategory({ category, operationType: "fetchProductsByCategory" }));
-  };
+    const handleClickForCategory = (e: React.MouseEvent<HTMLElement>, category: CategorySingle) => {
+        e.preventDefault();
+        if (category === selectedCategory) {
+           return dispatch(setCategory(''));
+        }
+
+        if (category === 'all') {
+            dispatch(setCategory(category));
+            dispatch(setPage(1));
+            dispatch(fetchProducts({ operationType: "fetchProducts" }));
+            return 
+        }
+        dispatch(setCategory(category));
+        dispatch(setPage(1));
+        dispatch(fetchProductsByCategory({ category, operationType: "fetchProductsByCategory" }));
+    };
 
   return (
     <button className="hover:text-yellow-500 transition cursor-pointer" onClick={(e) => handleClickForCategory(e, category)}>
